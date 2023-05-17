1e955b5283b97404b21a7f59e71d5dffd9372bcc
choephix <choephix@gmail.com>
2023-05-17 19:03:37 +0300

    2 files modified

--- components/SmartTaskCard.tsx
+++ components/SmartTaskCard.tsx
@@ -13,20 +13,20 @@ export function SmartTaskCard(props: { task: TaskStore }) {
   }, [task]);
 
   return (
-    <div className='card with-border'>
-      <div className='card-header'>
-        <p className='card-question'>{task.question}</p>
-        <code className='card-text'>Status: {task.status}</code>
+    <div className="card with-border">
+      <div className="card-header">
+        <p className="card-question">{task.question}</p>
+        <code className="card-text">Status: {task.status}</code>
       </div>
 
       <hr />
 
-      <div className='card-body pretty-scrollbar' ref={cardBodyRef}>
+      <div className="card-body pretty-scrollbar" ref={cardBodyRef}>
         {task.initial_responses.length > 0 && (
-          <div className='initial-responses'>
-            <h5 className='card-subtitle mb-2 text-muted'>Initial Responses</h5>
+          <div className="initial-responses">
+            <h5 className="card-subtitle mb-2 text-muted">Initial Responses</h5>
             {task.initial_responses.map((response, index) => (
-              <div key={index} className='initial-response subcard'>
+              <div key={index} className="initial-response subcard">
                 {response}
               </div>
             ))}
@@ -34,17 +34,19 @@ export function SmartTaskCard(props: { task: TaskStore }) {
         )}
 
         {task.initial_prompt && (
-          <div className='initial-prompt'>
-            <h5 className='card-subtitle mb-2 text-muted'>Initial Prompt</h5>
-            <p className='card-text subcard'>{task.initial_prompt}</p>
+          <div className="initial-prompt">
+            <h5 className="card-subtitle mb-2 text-muted">Initial Prompt</h5>
+            <p className="card-text subcard">{task.initial_prompt}</p>
           </div>
         )}
 
         {task.researcher_responses.length > 0 && (
-          <div className='researcher-responses'>
-            <h5 className='card-subtitle mb-2 text-muted'>Researcher Responses</h5>
+          <div className="researcher-responses">
+            <h5 className="card-subtitle mb-2 text-muted">
+              Researcher Responses
+            </h5>
             {task.researcher_responses.map((response, index) => (
-              <div key={index} className='researcher-response subcard'>
+              <div key={index} className="researcher-response subcard">
                 <strong>{response.role}:</strong> {response.content}
               </div>
             ))}
@@ -52,23 +54,24 @@ export function SmartTaskCard(props: { task: TaskStore }) {
         )}
 
         {task.final_response && (
-          <div className='final-response'>
-            <h5 className='card-subtitle mb-2 text-muted'>Final Response</h5>
-            <p className='card-text subcard'>{task.final_response}</p>
-          </div>
-        )}
-
-        {task.perfect_result && (
-          <div className='perfect-result'>
-            <h5 className='card-subtitle mb-2 text-muted'>Perfect Result</h5>
-            <p className='card-text subcard'>{task.perfect_result}</p>
+          <div className="final-response">
+            <h5 className="card-subtitle mb-2 text-muted">Final Response</h5>
+            <p className="card-text subcard">{task.final_response}</p>
           </div>
         )}
       </div>
 
       <hr />
 
-      <div className='card-footer'>{task.ongoing ? <progress id='working' /> : null}</div>
+      <div className="card-footer">
+        {task.ongoing ? <progress id="working" /> : null}
+        {task.perfect_result && (
+          <div className="perfect-result">
+            <h5 className="card-subtitle mb-2 text-muted">Perfect Result</h5>
+            <p className="card-text subcard">{task.perfect_result}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
