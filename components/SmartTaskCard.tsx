5e5163a6085cb613896161c317b88b02223a4aa0
choephix <choephix@gmail.com>
2023-05-18 01:35:39 +0300

    2 files modified

--- components/SmartTaskCard.tsx
+++ components/SmartTaskCard.tsx
@@ -19,48 +19,54 @@ export function SmartTaskCard(props: { task: TaskStore }) {
     <div className="card with-border">
       <div className="card-header">
         <p className="card-question">{task.question}</p>
-        <code className="card-status card-text">Status: {task.status}</code>
+        <code className="card-status card-text">
+          Status: {task.statusMessage}
+        </code>
       </div>
 
-      <div className="card-body pretty-scrollbar" ref={cardBodyRef}>
-        {task.initial_responses.length > 0 && (
-          <div className="initial-responses">
-            <h5 className="card-subtitle mb-2 text-muted">Initial Responses</h5>
-            {task.initial_responses.map((response, index) => (
-              <div key={index} className="initial-response subcard">
-                {response}
-              </div>
-            ))}
-          </div>
-        )}
+      {(task.phase || 0) < 2 ? null : (
+        <div className="card-body pretty-scrollbar" ref={cardBodyRef}>
+          {task.initial_responses.length > 0 && (
+            <div className="initial-responses">
+              <h5 className="card-subtitle mb-2 text-muted">
+                Initial Responses
+              </h5>
+              {task.initial_responses.map((response, index) => (
+                <div key={index} className="initial-response subcard">
+                  {response}
+                </div>
+              ))}
+            </div>
+          )}
 
-        {task.initial_prompt && (
-          <div className="initial-prompt">
-            <h5 className="card-subtitle mb-2 text-muted">Initial Prompt</h5>
-            <p className="card-text subcard">{task.initial_prompt}</p>
-          </div>
-        )}
+          {task.initial_prompt && (
+            <div className="initial-prompt">
+              <h5 className="card-subtitle mb-2 text-muted">Initial Prompt</h5>
+              <p className="card-text subcard">{task.initial_prompt}</p>
+            </div>
+          )}
 
-        {task.researcher_responses.length > 0 && (
-          <div className="researcher-responses">
-            <h5 className="card-subtitle mb-2 text-muted">
-              Researcher Responses
-            </h5>
-            {task.researcher_responses.map((response, index) => (
-              <div key={index} className="researcher-response subcard">
-                <strong>{response.role}:</strong> {response.content}
-              </div>
-            ))}
-          </div>
-        )}
+          {task.researcher_responses.length > 0 && (
+            <div className="researcher-responses">
+              <h5 className="card-subtitle mb-2 text-muted">
+                Researcher Responses
+              </h5>
+              {task.researcher_responses.map((response, index) => (
+                <div key={index} className="researcher-response subcard">
+                  <strong>{response.role}:</strong> {response.content}
+                </div>
+              ))}
+            </div>
+          )}
 
-        {task.final_response && (
-          <div className="final-response">
-            <h5 className="card-subtitle mb-2 text-muted">Final Response</h5>
-            <p className="card-text subcard">{task.final_response}</p>
-          </div>
-        )}
-      </div>
+          {task.final_response && (
+            <div className="final-response">
+              <h5 className="card-subtitle mb-2 text-muted">Final Response</h5>
+              <p className="card-text subcard">{task.final_response}</p>
+            </div>
+          )}
+        </div>
+      )}
 
       <div
         className={
--- lib/useSmartGPT.ts
+++ lib/useSmartGPT.ts
@@ -10,7 +10,8 @@ function createTaskStore(question: string) {
     question,
     outputs_count: 3,
     ongoing: true,
-    status: 'Initializing...',
+    phase: 0,
+    statusMessage: 'Initializing...',
     initial_responses: [] as string[],
     initial_prompt: '',
     researcher_responses: [] as { role: string; content: string }[],
@@ -22,7 +23,9 @@ function createTaskStore(question: string) {
 export type TaskStore = ReturnType<typeof createTaskStore>;
 
 async function findThePerfectResult(smartGPT: SmartGPT, store: TaskStore) {
-  store.status = 'Generating initial answers';
+  store.statusMessage = 'Generating initial answers';
+
+  store.phase = 1;
 
   [store.initial_responses, store.initial_prompt] =
     await smartGPT.getInitialResponses(
@@ -31,21 +34,27 @@ async function findThePerfectResult(smartGPT: SmartGPT, store: TaskStore) {
       (response) => store.initial_responses = [...store.initial_responses, response]
     );
 
+  store.phase = 2;
+
   const answers = smartGPT.concatenateResponses(store.initial_responses);
 
-  store.status = `Researching answers: 2/4 complete`;
+  store.statusMessage = `Researching answers: 2/4 complete`;
   store.researcher_responses = await smartGPT.getResearcherResponses(
     answers,
     store.initial_prompt,
     store.outputs_count
   );
 
-  store.status = `Resolving answers: 3/4 complete`;
+  store.phase = 3;
+
+  store.statusMessage = `Resolving answers: 3/4 complete`;
   store.final_response = await smartGPT.getResolverResponse(
     store.researcher_responses,
     store.outputs_count
   );
   store.perfect_result = await smartGPT.getImprovedResponse(store.final_response);
+
+  store.phase = 4;
 
   // update tokens and calculate total cost
   const total_calc =
@@ -56,7 +65,7 @@ async function findThePerfectResult(smartGPT: SmartGPT, store: TaskStore) {
   console.log(`You used ${smartGPT.tokenCounts[gpt3]} gpt3.5 tokens`);
   console.log(`You used ${smartGPT.tokenCounts[gpt4]} gpt4 tokens`);
 
-  store.status = `🎈 Ready! This query cost you ${total_cost}`;
+  store.statusMessage = `🎈 Ready! This query cost you ${total_cost}`;
   store.ongoing = false;
 }
 
